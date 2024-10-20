b1d84c88e2a3230cbf996ab2f1a2ee8beb86a898
Mohd-Affan-Code <mohdaffanjkhan@gamil.com>
2024-10-20 12:42:31 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import SingUp from "./Component/SingUp";
+
+const App = () => {
+  return (
+    <div>
+      <SingUp />
+    </div>
+  );
+};
+
+export default App;
